16693e4d3abdbb4fc8965f302ef7b61122bded5a
Sebastian Gössl <goessl@student.tugraz.at>
2026-06-17 10:11:41 +0200

    Tabulator.js minimal setup

--- hooks/build_data.py
+++ hooks/build_data.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 from datetime import date
 import yaml
-import pandas as pd
+import json
 
 
 ROOT = Path(__file__).resolve().parent.parent
 SOURCE = ROOT / "bibliography.yml"
-TARGET = ROOT / "docs" / "bibliography.md"
+TARGET = ROOT / "docs" / "bibliography.json"
 
 BOOKLET_FIELDS = {'author', 'title'}
 BOOK_FIELDS = {'author', 'title', 'date', 'edition', 'publisher', 'isbn', 'doi', 'url'}
@@ -48,44 +48,5 @@ def on_pre_build(config, **kwargs):
                             raise TypeError(f'Wrong format: {entry}')
         
         
-        #tabulate
-        df = pd.DataFrame.from_dict(data)
-        
-        
-        #format
-        #column sorting
-        PRIO_COLUMNS = ('type', 'id', 'author', 'title', 'subtitle', 'url')
-        columns = [c for c in df.columns if c in PRIO_COLUMNS]
-        columns += [c for c in df.columns if not c in PRIO_COLUMNS]
-        df = df[columns]
-        #edition from float to Int64 (allows NA)
-        df = df.convert_dtypes()
-        #html
-        if "author" in df.columns:
-            df["author"] = df["author"].apply(
-                lambda x: '<ul>'+''.join(f'<li>{e}</li>' for e in x)+'</ul>'# if pd.notna(x) else x
-            )
-        if "doi" in df.columns:
-            df["doi"] = df["doi"].apply(
-                lambda x: f'<a href="https://doi.org/{x}" target="_blank">{x}</a>' if pd.notna(x) else x
-            )
-        if "url" in df.columns:
-            df["url"] = df["url"].apply(
-                lambda x: f'<a href="{x}" target="_blank">{x}</a>' if pd.notna(x) else x
-            )
-        #prepare NA cells as empty string
-        df = df.astype('object').where(df.notna(), '')
-        
-        
         #write
-        ofile.write('---\n'
-                    'hide:\n'
-                    '  - toc\n'
-                    'extra:\n'
-                    '  layout: full\n'
-                    '---\n'
-                    '\n'
-                    '# Bibliography\n'
-                    '\n'
-        )
-        df.to_markdown(ofile, index=False)
+        json.dump(data, ofile, ensure_ascii=False, default=str)
