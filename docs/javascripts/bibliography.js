34da9d65f638eb8e8385bfdb6e6a117193002fe0
Sebastian Gössl <goessl@student.tugraz.at>
2026-06-17 10:43:12 +0200

    filters

--- docs/javascripts/bibliography.js
+++ docs/javascripts/bibliography.js
@@ -58,8 +58,71 @@ var data = [
 ];
 
 //autocolumns would only scan first row
-var columns = [...new Set(data.flatMap(Object.keys))].map(function(key) {
-    return {field: key, title: key};
+var fields = [...new Set(data.flatMap(Object.keys))];
+var authors = [...new Set(data.flatMap(function (e) { return e.author || []; }))].sort();
+
+//define formatting and filtering for each column
+var columns = fields.map(function (field) {
+  var column = {title: field,
+                field: field };
+  
+  switch (field) {
+    case "type":
+      column.headerFilter = "list";
+      column.headerFilterParams = {valuesLookup: true,
+                                   clearable: true,
+                                   multiselect: true };
+      column.headerFilterFunc = "in";
+      break;
+    
+    case "author":
+      column.formatter = function (cell) {
+        var values = cell.getValue() || [];
+        return (
+          "<ul>" +
+          values.map(function (a) { return "<li>"+a+"</li>"; }).join("") +
+          "</ul>"
+        );
+      };
+      
+      column.headerFilter = "list";
+      column.headerFilterParams = {values: authors, clearable: true, multiselect: true};
+      column.headerFilterFunc = function (headerValue, rowValue) {
+        if (!headerValue || !headerValue.length) return true;
+        return (rowValue || []).some(function (a) { return headerValue.includes(a); });
+      };
+      break;
+    
+    case "url":
+      column.formatter = "link";
+      column.formatterParams = {label: "link",
+                                target: "_blank" };
+      break;
+    
+    case "publisher":
+      column.headerFilter = "list";
+      column.headerFilterParams = {valuesLookup: true,
+                                   clearable: true,
+                                   multiselect: true };
+      column.headerFilterFunc = "in";
+      break;
+    
+    case "doi":
+      column.formatter = "link";
+      column.formatterParams = {
+        label: function (cell) { return cell.getValue(); },
+        url: function (cell) { return "https://doi.org/" + cell.getValue(); },
+        target: "_blank"
+      };
+      column.headerFilter = "input";
+      break;
+    
+    default:
+      column.headerFilter = "input";
+      break;
+  }
+
+  return column;
 });
 
 new Tabulator("#bibliography-table", {
